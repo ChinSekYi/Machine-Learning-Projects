919b49b0b4171d024cae1cad498155474ff8e9eb
Chin Sek Yi <sekyichin@gmail.com>
2024-03-24 14:57:59 +0800

    update files

--- code/data_processing.py
+++ code/data_processing.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
-'''
+"""
 functions starting with df_ can generate a processed dataframe directly
-'''
+"""
+
 
 def as_discrete(col):
     n = len(col)
@@ -35,14 +38,16 @@ def med_impute(df, y):
     df = df[df.isnull().sum(axis=1) <= thd2]
 
     # median imputation for null values
-    df.fillna(df.median())
+    df = df.fillna(df.median())
 
     return df, y
 
 
 def normalise(df):
     scaler = MinMaxScaler()
-    X_scaled_df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+    X_scaled = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+
+    return X_scaled
 
 
 def df_null_removal(df):
@@ -59,7 +64,6 @@ def df_null_removal(df):
 
 
 def drop_high_corr(df, threshold=0.7):
-
     correlation_matrix = df.corr()
     high_cor = []
     dropped_features = []
@@ -69,11 +73,13 @@ def drop_high_corr(df, threshold=0.7):
         for j in range(i):
             if abs(correlation_matrix.iloc[i, j]) > threshold:
                 if correlation_matrix.columns[j] != correlation_matrix.columns[i]:
-                    high_cor.append([
-                        correlation_matrix.columns[i],
-                        correlation_matrix.columns[j],
-                        correlation_matrix.iloc[i, j]
-                    ])
+                    high_cor.append(
+                        [
+                            correlation_matrix.columns[i],
+                            correlation_matrix.columns[j],
+                            correlation_matrix.iloc[i, j],
+                        ]
+                    )
 
     # Iterate through the list of highly correlated pairs
     for pair in high_cor:
@@ -88,17 +94,58 @@ def drop_high_corr(df, threshold=0.7):
                 df.drop(feature2, axis=1, inplace=True)
                 dropped_features.append(feature2)
             else:
-                print(f"Feature '{feature2}' not found in the DataFrame.")
+                # print(f"Feature {feature2} not found in the DataFrame.")
+                print(
+                    "Feature '" + feature2 + "' not found in the DataFrame."
+                )  # temporary
 
     return df
 
+
 def df_null_corr_process(df):
-    return drop_high_corr(df_null_removal(df))
+    X, y = df_null_removal(df)
+    return drop_high_corr(X), y
 
 
 def pre_process(df):
     X, y = get_Xy(df)
     X_imputed, y_final = med_impute(X, y)
     X_scaled = normalise(X_imputed)
+    print(X_scaled.shape)
     X_final = drop_high_corr(X_scaled)
+
     return X_final, y_final
+
+
+def get_train_test(df):
+
+    X, y = get_Xy(df)
+    X_imputed, y_final = med_impute(X, y)
+    X_scaled = normalise(X_imputed)
+    X_final = drop_high_corr(X_scaled)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_final, y_final, test_size=0.2, random_state=3244
+    )
+
+    return X_train, X_test, y_train, y_test
+
+
+def get_df_with_top_k_features(k_features, X_train, X_test, y_train, y_test):
+    # define feature selection
+    fs = SelectKBest(score_func=f_classif, k=k_features)
+
+    # apply feature selection
+    X_selected = fs.fit_transform(X_train, y_train)
+
+    # Take the features with the highest F-scores
+    fs_scores_array = np.array(fs.scores_)
+
+    # Get the indices that would sort the array in descending order
+    sorted_indices_desc = np.argsort(fs_scores_array)[::-1]
+
+    # Take the top k indices
+    top_indices = sorted_indices_desc[:k_features]
+
+    X_train_with_selected_x = X_train.iloc[:, top_indices]
+    X_test_with_selected_x = X_test.iloc[:, top_indices]
+    return X_train_with_selected_x, X_test_with_selected_x, y_train, y_test
