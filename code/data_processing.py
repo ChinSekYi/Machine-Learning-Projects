98bc088fb3870409f551b9899a3d819259440fcc
kiwi-lemongrass <144644764+kiwi-lemongrass@users.noreply.github.com>
2024-03-24 11:36:26 +0800

    Update data_processing.py

--- code/data_processing.py
+++ code/data_processing.py
@@ -13,32 +13,27 @@ def as_discrete(col):
     return pd.DataFrame(new_col)
 
 def get_Xy(df):
-    X = df_origin.iloc[:, 0:len(df)-1]
+    X = df.iloc[:, 0:len(df)-1]
     y = as_discrete(df.iloc[:, -1])
     return X, y
 
+def med_impute(df, y):
+    # remove columns with more than 40% values being null
+    thd1 = df.shape[0] * 0.4
+    cols = df.columns[df.isnull().sum() < thd1]
+    df = df[cols]
+
+    # remove rows with more than 50% values being null
+    thd2 = df.shape[1] * 0.5
+    y = y[df.isnull().sum(axis=1) <= thd2]
+    df = df[df.isnull().sum(axis=1) <= thd2]
+
+    # median imputation for null values
+    df.fillna(df.median())
+
+    return df, y
+
 def null_removal(df):
-    # Define a function to convert binary columns to discrete
-    def as_discrete(col):
-        n = len(col)
-        new_col = [0] * n
-        for i in range(n):
-            if col[i] == b'0':
-                new_col[i] = 0
-            else:
-                new_col[i] = 1
-        return pd.DataFrame(new_col)
-
-    # Extract features (X) and target (y)
-    X = df.iloc[:, :-1]
-    y = as_discrete(df.iloc[:, -1])
-
-    # Remove rows where more than half of the entries are null
-    threshold = X.shape[1] / 2  # Total number of columns divided by 2
-    X_clean = X[X.isnull().sum(axis=1) <= threshold]
-
-    # Impute missing values with the median
-    X_imputed = X_clean.fillna(X_clean.median())
     scaler = MinMaxScaler()
 
     X_scaled = scaler.fit_transform(df)
