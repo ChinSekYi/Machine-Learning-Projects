dcc6e489bc151e95ffb2be8297c53c996cd55dd6
Chin Sek Yi <sekyichin@gmail.com>
2024-04-07 16:55:10 +0800

    lint changes

--- code/test_csy.py
+++ code/test_csy.py
@@ -1,4 +1,4 @@
-import dtreeviz as dt
+import dtreeviz as dtree
 import graphviz.backend as be
 from IPython.display import SVG, Image, display_svg
 from sklearn.datasets import *
@@ -6,17 +6,21 @@ from sklearn.tree import DecisionTreeClassifier
 
 clas = DecisionTreeClassifier(max_depth=2)
 iris = load_iris()
-print(dir(dt.trees))
+print(dir(dtree.trees))
+
 
 X_train = iris.data
+print(type(X_train))
 y_train = iris.target
+print(type(iris.feature_names))
+
 clas.fit(X_train, y_train)
 from sklearn.tree import plot_tree
 
 plot_tree(clas)
 
 # 1. Classification
-viz = dt.trees.model(
+viz = dtree.trees.model(
     clas,
     X_train,
     y_train,
