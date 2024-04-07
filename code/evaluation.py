03b984c4e1aa0fb049e64318bdf9d919da909807
kiwi-lemongrass <144644764+kiwi-lemongrass@users.noreply.github.com>
2024-04-07 12:25:21 +0800

    Update evaluation.py

--- code/evaluation.py
+++ code/evaluation.py
@@ -3,7 +3,8 @@ from sklearn.metrics import confusion_matrix, classification_report
 
 def print_res(y_test, y_pred):
     print("Accuracy:", accuracy_score(y_test, y_pred))
-    print("F1 Score:", f1_score(y_test, y_pred))
+    print("Precision Score:", precision_score(y_test, y_pred))
     print("Recall Score:", recall_score(y_test, y_pred))
+    print("F1 Score:", f1_score(y_test, y_pred))
     print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
     print("Classification Report:\n", classification_report(y_test, y_pred))
