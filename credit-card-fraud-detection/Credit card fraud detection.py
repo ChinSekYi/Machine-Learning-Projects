430cf30bb201b8f6f04510cd7a9d2755cf7a17cd
Chin Sek Yi <sekyichin@gmail.com>
2024-04-24 22:49:53 +0800

    lint changes

--- credit-card-fraud-detection/Credit card fraud detection.py
+++ credit-card-fraud-detection/Credit card fraud detection.py
@@ -1,58 +1,62 @@
-import pandas as pd #to process csv files & make dataframes
-import numpy as np #for making arrays
-from sklearn.model_selection import train_test_split    #pip install -U scikit-learn
+import numpy as np  # for making arrays
+import pandas as pd  # to process csv files & make dataframes
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import \
+    train_test_split  # pip install -U scikit-learn
 
-
-#load dataset to dataframe
-credit_card_data = pd.read_csv('creditcard.csv')
+# load dataset to dataframe
+credit_card_data = pd.read_csv("creditcard.csv")
 credit_card_data.head()
 
-#credit_card_data.info()
+# credit_card_data.info()
 
-#check number of missing values in each column 
+# check number of missing values in each column
 sum = credit_card_data.isnull().sum()
 
-#distribution of legit transactions and fraudulent transactions
-count = credit_card_data['Class'].value_counts()
+# distribution of legit transactions and fraudulent transactions
+count = credit_card_data["Class"].value_counts()
 
-#Comment: very unbalanced dataset -> cannot use it to feed ML model because model cannot recognise fraudulent transactions due to very less fraudulent data
+# Comment: very unbalanced dataset -> cannot use it to feed ML model because model cannot recognise fraudulent transactions due to very less fraudulent data
 
-#separating data for analysis
+# separating data for analysis
 legit = credit_card_data[credit_card_data.Class == 0]
 fraud = credit_card_data[credit_card_data.Class == 1]
 
-#statistical measures 
+# statistical measures
 ldes = legit.Amount.describe()
 fdes = fraud.Amount.describe()
 
-#compare values for both transactions
-mean = credit_card_data.groupby('Class').mean()
+# compare values for both transactions
+mean = credit_card_data.groupby("Class").mean()
 
 # under-sampling: build a sample dataset containing similar distribution of normal transactions and fraudulent transactions
 legit_sample = legit.sample(n=492)
 
-new_dataset = pd.concat([legit_sample,fraud], axis=0)
+new_dataset = pd.concat([legit_sample, fraud], axis=0)
 
-n = new_dataset['Class'].value_counts()
+n = new_dataset["Class"].value_counts()
 
-new_mean = new_dataset.groupby('Class').mean() # good sample occurs when the mean before and after sample isnt too different
+new_mean = new_dataset.groupby(
+    "Class"
+).mean()  # good sample occurs when the mean before and after sample isnt too different
 
-#splitting the data into features and types
-X = new_dataset.drop(columns = 'Class', axis=1)
-Y = new_dataset['Class']
+# splitting the data into features and types
+X = new_dataset.drop(columns="Class", axis=1)
+Y = new_dataset["Class"]
 
-# split data into training data & testing data 
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2,stratify=Y, random_state=2) #stratify -> evenly split class 0/1 in training and test dataset #random state -> generator used to shuffle data
+# split data into training data & testing data
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.2, stratify=Y, random_state=2
+)  # stratify -> evenly split class 0/1 in training and test dataset #random state -> generator used to shuffle data
 
-#Model training - Logistic regression
+# Model training - Logistic regression
 model = LogisticRegression()
 
-#training model
-model.fit(X_train,Y_train)
+# training model
+model.fit(X_train, Y_train)
 
-#model evaluation - accuracy score
+# model evaluation - accuracy score
 X_train_prediction = model.predict(X_train)
 training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
 print(training_data_accuracy)
@@ -62,4 +66,4 @@ X_test_prediction = model.predict(X_test)
 test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
 print(test_data_accuracy)
 
-#Comment: if scores between training and test data is very different, the model might be underfitted or overfitted
+# Comment: if scores between training and test data is very different, the model might be underfitted or overfitted
--- image-classification/Hymenoptera_scratch(ignore).py
+++ image-classification/Hymenoptera_scratch(ignore).py
@@ -1,276 +1,149 @@
-import matplotlib.pyplot as plt 
-import numpy as np 
-import os 
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torchvision import datasets, models
+
+data_transforms = {
+    "train": transforms.Compose(
+        [
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    ),
+    "val": transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    ),
+}
+
+
+batch_size = 424
+num_workers = 4
+
+data_dir = "/.../tutorial/hymenoptera_data"
+
+image_datasets = {
+    x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+    for x in ["train", "val"]
+}
+
+dataloaders = {
+    x: torch.utils.data.DataLoader(
+        image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=num_workers
+    )
+    for x in ["train", "val"]
+}
+
+trainset = dataloaders["train"]
+testset = dataloaders["val"]
+
+dataset_sizes = {x: len(image_datasets[x]) for x in ["train", "val"]}
+class_names = image_datasets["train"].classes  # ['ants', 'bees']
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def imshow(inp, title=None):
+    """Display image for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+# Get a batch of training data
+inputs, classes = next(iter(dataloaders["train"]))
+
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+
+imshow(out, title=[class_names[x] for x in classes])
+
+
+# Define CNN: models a simple Convolutional NN
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.flatten(1)
+        # x = x.view(-1, 16 * 5* 5) #to reshape tensor for our fc # or use x.flatten(1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
-import torch 
-import torchvision 
-from torchvision import datasets, models 
-import torchvision.transforms as transforms 
-import torch.nn as nn 
-import torch.nn.functional as F 
-import torch.optim as optim 
 
-data_transforms = { 
+net = Net()
+print(net)
 
-    'train': transforms.Compose([ 
+# Define a loss function and optimiser
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-        transforms.RandomResizedCrop(224), 
+for epoch in range(2):
+    running_loss = 0.0
 
-        transforms.RandomHorizontalFlip(), 
+    for i, data in enumerate(trainset, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
 
-        transforms.ToTensor(), 
-
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) 
-
-    ]), 
-
-    'val': transforms.Compose([ 
-
-        transforms.Resize(256), 
-
-        transforms.CenterCrop(224), 
-
-        transforms.ToTensor(), 
-
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) 
-
-    ]), 
-
-} 
-
-  
-batch_size = 424 
-num_workers = 4 
-
-data_dir = "/.../tutorial/hymenoptera_data" 
-
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), 
-                                          data_transforms[x]) 
-                  for x in ['train', 'val']} 
-
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, 
-
-                                             shuffle=True, num_workers=num_workers) 
-              for x in ['train', 'val']} 
-
-trainset = dataloaders['train'] 
-
-testset = dataloaders['val'] 
-
-  
-
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']} 
-
-  
-
-class_names = image_datasets['train'].classes #['ants', 'bees'] 
-
-  
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
-
- 
-
- 
-
- 
-
- 
-
-def imshow(inp, title=None): 
-
-    """Display image for Tensor.""" 
-
-    inp = inp.numpy().transpose((1, 2, 0)) 
-
-    mean = np.array([0.485, 0.456, 0.406]) 
-
-    std = np.array([0.229, 0.224, 0.225]) 
-
-    inp = std * inp + mean 
-
-    inp = np.clip(inp, 0, 1) 
-
-    plt.imshow(inp) 
-
-    if title is not None: 
-
-        plt.title(title) 
-
-    plt.pause(0.001)  # pause a bit so that plots are updated 
-
-  
-
-  
-
-# Get a batch of training data 
-
-inputs, classes = next(iter(dataloaders['train'])) 
-
-  
-
-# Make a grid from batch 
-
-out = torchvision.utils.make_grid(inputs) 
-
-  
-
-imshow(out, title=[class_names[x] for x in classes]) 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
-#Define CNN: models a simple Convolutional NN 
-
-  
-
-class Net(nn.Module): 
-    def __init__(self): 
-        super(Net, self).__init__() 
-        self.conv1 = nn.Conv2d(3,6,5) 
-        self.pool = nn.MaxPool2d(2,2)  
-        self.conv2 = nn.Conv2d(6,16,5) 
-        self.fc1 = nn.Linear(16 * 5 * 5, 120) 
-        self.fc2 = nn.Linear(120, 84) 
-        self.fc3 = nn.Linear(84, 10) 
-
-    def forward(self, x): 
-        x = self.pool(F.relu(self.conv1(x))) 
-
-        x = self.pool(F.relu(self.conv2(x))) 
-
-        x = x.flatten(1) 
-
-        #x = x.view(-1, 16 * 5* 5) #to reshape tensor for our fc # or use x.flatten(1) 
-
-        x = F.relu(self.fc1(x)) 
-
-        x = F.relu(self.fc2(x)) 
-
-        x = self.fc3(x) 
-
-        return x  
-
-     
-
-net = Net() 
-
-print(net) 
-
- 
-
- 
-
- 
-
-#Define a loss function and optimiser 
-
-criterion = nn.CrossEntropyLoss() 
-
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9) 
-
- 
-
- 
-
- 
-
-for epoch in range(2):  
-
-  
-
-    running_loss = 0.0 
-
-    for i, data in enumerate(trainset, 0): 
-
-        inputs, labels = data  
-
-        optimizer.zero_grad()  
-
-  
-
-        outputs = net(inputs) 
-
-        loss = criterion(outputs, labels) 
-
-  
-
-        loss.backward()   
-
-        optimizer.step() 
-
-  
-
-        #print statistics 
-
-        running_loss += loss.item() 
-
+        running_loss += loss.item()
         """ 
-
         if i % 2000 == 1999: #mini-batch size is 2000 
-
             print('[%d, %5d] loss: %.3f' % 
-
                   (epoch + 1, i + 1, running_loss / 2000)) 
-
             running_loss = 0.0 
+        """
 
-        """ 
 
-  
+# waits for everything to finish running
+torch.cuda.synchronize()
+print("Finished Training")
 
-  
+correct = 0
+total = 0
+running_corrects = 0
 
-# waits for everything to finish running 
+with torch.no_grad():  # used when we dont require Pytorch to run its autograd engine/calculate the gradients of our input.
+    for i, data in enumerate(testset):
+        images, labels = data
+        outputs = net(images)  # gives the output tensor
+        _, predicted = torch.max(
+            outputs.data, 1
+        )  # _ gives the index and predicted gives the values of the max element
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-torch.cuda.synchronize() 
-
-  
-
-print('Finished Training') 
- 
-
- 
-
- 
-
- 
- 
-correct = 0 
-
-total = 0 
-
-running_corrects = 0 
-
-  
-
-with torch.no_grad(): #used when we dont require Pytorch to run its autograd engine/calculate the gradients of our input. 
-
-    for i, data in enumerate(testset): 
-
-        images, labels = data 
-
-        outputs = net(images) #gives the output tensor 
-
-        _, predicted = torch.max(outputs.data, 1) #_ gives the index and predicted gives the values of the max element 
-
-  
-
-        total += labels.size(0) 
-
-        correct += (predicted == labels).sum().item() 
-
-  
-
-  
-
-print("Accuracy of the network on the 10000 test images: %d", 100 * correct / total) 
+print("Accuracy of the network on the 10000 test images: %d", 100 * correct / total)
